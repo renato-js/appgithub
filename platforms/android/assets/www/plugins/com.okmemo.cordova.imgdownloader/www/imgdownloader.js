ea9840426e14d28bb4d8b130cd3d6a1b3565b1b9
Renato <renato@onwebstudio.com.br>
2016-10-09 03:51:20 -0300

    first
    v1.0 release

---
+++ platforms/android/assets/www/plugins/com.okmemo.cordova.imgdownloader/www/imgdownloader.js
@@ -0,0 +1,17 @@
+cordova.define("com.okmemo.cordova.imgdownloader.imgDownloader", function(require, exports, module) {
+var cordova = require('cordova');
+
+ImgDownloader = function(){}
+
+ImgDownloader.prototype.download = function(src,onsuccess,onerror){
+	cordova.exec(onsuccess, onerror, "ImgDownloader", "download", [src]);
+};
+
+ImgDownloader.prototype.downloadWithUrl = function(url,onsuccess,onerror){
+	cordova.exec(onsuccess, onerror, "ImgDownloader", "downloadWithUrl", [url]);
+};
+
+// Register the plugin
+var imgDownloader = new ImgDownloader();
+module.exports = imgDownloader;
+});
